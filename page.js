4f990737b89141498c1fb6b2becad47d16016b53
gantengezar19-sys <gantengezar19@gmail.com>
2026-02-17 15:33:53 +0700

    Add files via upload

--- page.js
+++ page.js
@@ -1,14 +1,14 @@
-import './globals.css';
-
 export const metadata = {
-  title: 'Oce Cuci Express',
-  description: 'Laundry Cepat, Bersih & Wangi - Gratis Antar Jemput Maks. 3KM',
+  title: "OCE Cuci Express",
+  description: "Laundry Express Profesional",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="id">
-      <body>{children}</body>
+      <body style={{margin:0,fontFamily:'Arial, sans-serif',scrollBehavior:'smooth'}}>
+        {children}
+      </body>
     </html>
   );
 }
